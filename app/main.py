71d9033e6ec40dcd01673c5a7a5fc689320bd9ce
juanca82 <juanocampoz.jc@gmail.com>
2022-12-02 12:26:14 +0100

    instalando docker

--- app/main.py
+++ app/main.py
@@ -8,16 +8,13 @@ def run():
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))'''
-  
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
   df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'South America']
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  
+  df - df[df['Continent'] == 'South America']
   charts.generate_pie_chart(countries, percentages)
-  data = read_csv.read_csv('data.csv')
   
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
