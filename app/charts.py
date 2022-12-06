96ce9374e5c057d72addeec97f5814f867752319
juanca82 <juanocampoz.jc@gmail.com>
2022-12-06 15:14:24 +0100

    finalizando curso

--- app/charts.py
+++ app/charts.py
@@ -10,7 +10,7 @@ def generate_pie_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.savefig(f'.imgs/{name}.png')
+  plt.savefig(f'.imgs/como,mor.png')
   plt.close()
 
 if __name__ == '__main__':
